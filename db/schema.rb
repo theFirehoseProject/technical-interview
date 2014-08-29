a4841a84b9337fd855b762fd8b5a117600ced01c
Ben Ricker <BGRicker@gmail.com>
2014-08-29 09:55:35 +0000

    add interview model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824201803) do
+ActiveRecord::Schema.define(version: 20140828233221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ test/helpers/admin/interviews_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::InterviewsHelperTest < ActionView::TestCase
+end

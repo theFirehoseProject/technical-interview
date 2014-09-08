b178433ad403efa8130d036cce45b1392e523375
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-09-08 09:45:16 +0000

    Generate admin dashboard page and redirect after login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!, :only => :welcome
 
   def index
-    if current_user.present?
+    if current_user.present? && current_user.admin?
+      redirect_to admin_path
+      elsif current_user.present?
       redirect_to welcome_path
     end
   end
@@ -10,5 +12,5 @@ class StaticPagesController < ApplicationController
   def welcome
 
   end
-
+  
 end

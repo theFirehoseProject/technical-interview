94ff59de0ded01d762125f1c4da91fd2bf91f479
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-09-15 08:56:57 +0000

    Add create form. Post still not working

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,4 +1,22 @@
 class Admin::QuestionsController < ApplicationController
 	def index
 	end
+
+	def create
+		@question = Question.create(question_params)
+   		if @question.invalid?
+      		flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    	end
+		redirect_to admin_dashboard_path
+	end
+
+	def new
+		@question = Question.new
+	end
+
+	private
+
+	def  question_params
+		params.require(:question).permit(:problem,:solution,:topic_id)
+	end
 end

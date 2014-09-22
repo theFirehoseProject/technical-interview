cdf8f9257f8edc683afb0d7125dfcc43570d5cd6
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-09-22 12:57:02 +0000

    Add create interviews modal on admin dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,6 +3,10 @@ class Admin::DashboardsController < ApplicationController
 	def show
 		@questions = Question.all
 		@question = Question.new
+
+		@intervies = Interview.all
+		@interview = Interview.new
+		
 		@topics = Topic.all
 		@users = User.all
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916154156) do
+ActiveRecord::Schema.define(version: 20140919110008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140916154156) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "question_id"
   end
 
   create_table "optins", force: true do |t|

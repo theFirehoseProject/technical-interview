3dd8a4f8e2a68cc1d46dbc563b3359c41f6cee8d
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-09-30 10:26:59 +0000

    Add optin test

--- test/controllers/optins_controller_test.rb
+++ test/controllers/optins_controller_test.rb
@@ -4,4 +4,8 @@ class OptinsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should redirect after optin" do
+		optin = Optin.create(:name => "Peter", :email => "peter@pan.com")
+		assert_response :success
+	end
 end

d62bd06da12d8655b6bdf144abfc974c5f838b8d
Ben Ricker <BGRicker@gmail.com>
2014-10-01 20:39:32 +0000

    stripe implemented with method error

---
+++ initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Examgoose::Application.config.secret_key_base = '104ef4862e4bd7af230c6ca3224b00e330c0405f6f000e9e77bf45b9a62d743e28f2bf71d4be6509c615d375770e7b237271a57d49ec298e71dd1b452ef0bb30'

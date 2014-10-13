a03fb314f47e215149932d9909b8a79671d1734c
Ben Ricker <BGRicker@gmail.com>
2014-10-13 11:19:57 +0000

    validations fix

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,6 +2,9 @@ class Interview < ActiveRecord::Base
 	has_and_belongs_to_many :questions
 	has_and_belongs_to_many :users
 
+	validates :title, length: { in: 5..50 }, :presence => true, :uniqueness => true
+	validates :description, length: { in: 20..300 }, :presence => true, :uniqueness => true
+
 	def claimed?(user)
 		self.users.include?(user)
 	end

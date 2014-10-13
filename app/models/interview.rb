bdef9c03b30d6efd95abf1f1b7e5abee6e3be0b4
Ben Ricker <BGRicker@gmail.com>
2014-10-13 01:02:48 +0000

    question/answer styles first take

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,9 +2,9 @@ class Interview < ActiveRecord::Base
 	has_and_belongs_to_many :questions
 	has_and_belongs_to_many :users
 
-	validates :title, :description, :presence => true, :uniqueness => true
-	validates :title, length: { in: 5..25 }
-	validates :description, length: { in: 20..200 }
+#	validates :title, :description, :presence => true, :uniqueness => true
+#	validates :title, length: { in: 5..50 }
+#	validates :description, length: { in: 20..300 }
 
 
 	def claimed?(user)

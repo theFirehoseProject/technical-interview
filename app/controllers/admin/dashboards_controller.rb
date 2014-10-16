0be4eb7927aa110a1e1ef244bfb5b7e120d98723
Angel Jose <ajose01@gmail.com>
2014-10-16 23:20:25 +0000

    Added authentication and verification of admin status to dashboard page

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardsController < ApplicationController
+before_action :authenticate_user!
+before_action :verify_admin
 
 	def show
 		@questions = Question.all
@@ -10,5 +12,39 @@ class Admin::DashboardsController < ApplicationController
 		@topics = Topic.all
 		@users = User.all
 	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private
+
+def verify_admin
+	unless current_user && current_user.admin?
+		flash[:alert] = "You are not authorized to access this page"
+		#render :text => "You are not authorized to access this page", :status => :forbidden
+		redirect_to root_path
+	end
+end
 
 end
